a241eb63c144fc90e6e3e8ab6ba4a1453a57c90b
mwgoh <gmw0924@gmail.com>
2017-12-25 10:51:34 +0900

    gmw test

--- gmw.java
+++ gmw.java
@@ -2,5 +2,10 @@ package gmw
 
 publc static void main(){
  private String gmw0924;
+ private String gmw0924;
+ private String gmw0924;
+ private String gmw0924;
+ private String gmw0924;
+ private String gmw0924;
 //
 }
